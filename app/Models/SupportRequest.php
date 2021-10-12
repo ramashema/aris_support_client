0a44931072cb9d77d376da84c66b37d074d99c35
Ramadhani Shemahonge <ramashema@gmail.com>
2021-10-12 21:19:08 +0300

    Added soft delete mechanism in support requests table

--- app/Models/SupportRequest.php
+++ app/Models/SupportRequest.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SupportRequest extends Model
 {
+    use SoftDeletes;
     use HasFactory;
+
     protected $fillable = [
         'user_id',
         'descriptions',
